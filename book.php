bddcb8457e44a0c60ed3560f8ef1476d688d6ec7
Hahn <dhahn@tamu.edu>
2023-04-14 09:57:30 -0500

    Update book.php

--- book.php
+++ book.php
@@ -7,21 +7,21 @@ $dirf = DIRF;
 $id = $_GET['id'];
 $first_letter = $id[0];
 
-	if (file_exists($dirf . $id . '/' . $id . '_meta.xml')) {
-		$meta = new SimpleXMLElement($dirf . $id . '/' . $id . '_meta.xml', NULL, TRUE);
+	if (file_exists($dirf . "/" . $id . '/' . $id . '_meta.xml')) {
+		$meta = new SimpleXMLElement($dirf . "/" . $id . '/' . $id . '_meta.xml', NULL, TRUE);
 		$title = $meta->title;
 	} else {
 		$title = 'title';
 	}
  
-	if (file_exists($dirf . $id . "/" . $id . '_abbyy.gz')) {
+	if (file_exists($dirf . "/" . $id . "/" . $id . '_abbyy.gz')) {
 		$search = 1;
 	} else {
 		$search = 0;
 	}
 
 BookReader::draw(BOOKREADER_HOSTNAME, 
-	$dirf . $id,
+	$dirf . "/" . $id,
 	$id,
 	'',
 	$title,
